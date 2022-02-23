c33f65153fb8175a31f77eb0383d5a6b79221a7d
verdulife <verdu@live.com>
2022-02-23 16:29:59 +0100

    testing template with ts

---
+++ src/routes/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { message } from '$lib/utils';
+
+	console.log(message);
+</script>
+
+<svelte:head>
+	<title>Home</title>
+</svelte:head>
+
+<h1>{message}</h1>
+
+<style lang="scss">
+	h1 {
+		color: $pri;
+	}
+</style>
